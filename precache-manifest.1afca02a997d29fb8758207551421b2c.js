2fddfa01ba9612c657002f08792de8524f526e65
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-11-06 14:18:47 +0200

    Updates

---
+++ precache-manifest.1afca02a997d29fb8758207551421b2c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771e80f1dfa5e1baf8b1c10bdd4fe025",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "17ec98a75de46fa4b901",
+    "url": "/my-portfolio/static/css/main.bbf4a09b.chunk.css"
+  },
+  {
+    "revision": "81943dc685fd125564e3",
+    "url": "/my-portfolio/static/js/2.6e7f76cd.chunk.js"
+  },
+  {
+    "revision": "5b4d2a656f298449adc16dc231737331",
+    "url": "/my-portfolio/static/js/2.6e7f76cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ec98a75de46fa4b901",
+    "url": "/my-portfolio/static/js/main.080df0a9.chunk.js"
+  },
+  {
+    "revision": "976294871add738ec1ea",
+    "url": "/my-portfolio/static/js/runtime-main.3134900d.js"
+  },
+  {
+    "revision": "2b9b7690ea41eca720fedddff070e153",
+    "url": "/my-portfolio/static/media/DancingScript-Medium.2b9b7690.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/my-portfolio/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/my-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b1ce4cd13e96c14fa23147273b48d523",
+    "url": "/my-portfolio/static/media/NanumBrushScript-Regular.b1ce4cd1.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/my-portfolio/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "56a9676b142056f818bf7fcb8c246350",
+    "url": "/my-portfolio/static/media/Vidaloka-Regular.56a9676b.ttf"
+  },
+  {
+    "revision": "36c6ea0ffc83bf1f4e5f4df8f2cdb77b",
+    "url": "/my-portfolio/static/media/css.36c6ea0f.png"
+  },
+  {
+    "revision": "db7933e6a7f7acaf99a33f39b286974b",
+    "url": "/my-portfolio/static/media/js.db7933e6.png"
+  },
+  {
+    "revision": "a007a312b856b9d4bc7d9f8a82221018",
+    "url": "/my-portfolio/static/media/me.a007a312.png"
+  },
+  {
+    "revision": "b9fa193e564dfd93617b79c2e405cc99",
+    "url": "/my-portfolio/static/media/meme.b9fa193e.PNG"
+  },
+  {
+    "revision": "23a01b46b4a6b907202c5b0b2bf6cbc4",
+    "url": "/my-portfolio/static/media/mines.23a01b46.PNG"
+  },
+  {
+    "revision": "1cc6bb251973afd6819ac7264013ba58",
+    "url": "/my-portfolio/static/media/mongo.1cc6bb25.png"
+  },
+  {
+    "revision": "4c6c0929cc58f241847a744b2de75e2f",
+    "url": "/my-portfolio/static/media/node.4c6c0929.png"
+  },
+  {
+    "revision": "cedf6c0bcea24bef375b62df98721933",
+    "url": "/my-portfolio/static/media/npm.cedf6c0b.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/my-portfolio/static/media/react.70f87bba.png"
+  },
+  {
+    "revision": "8e7bfce7da07e803bc337742c98b7914",
+    "url": "/my-portfolio/static/media/redux.8e7bfce7.png"
+  },
+  {
+    "revision": "195806df78701ff823827d3821db4f14",
+    "url": "/my-portfolio/static/media/sass.195806df.png"
+  },
+  {
+    "revision": "6925cb93ccc164417eaebf89f64fab65",
+    "url": "/my-portfolio/static/media/socket.6925cb93.png"
+  },
+  {
+    "revision": "2fc94e2ba69d3d028be751c586fffae9",
+    "url": "/my-portfolio/static/media/taskit.2fc94e2b.PNG"
+  },
+  {
+    "revision": "96e5850daed65a300d49b5f1acc9f120",
+    "url": "/my-portfolio/static/media/vue.96e5850d.png"
+  }
+]);
