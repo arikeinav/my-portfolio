5cd4a1f3f2a4d9762e2946f596fbab648e869636
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-11-06 18:14:54 +0200

    sssss

--- src/cmps/Preview.jsx
+++ src/cmps/Preview.jsx
@@ -7,7 +7,7 @@ export function Preview({project}) {
     <div className="preview-img" style={{backgroundImage:`url(${require(`../styles/images/${project.imgUrlName}.PNG`)})`}}></div>
      <div className="preview-info flex column"> <h3>{project.name}</h3>
       <p>{project.desc}</p>
-      <a href={`${project.url}`} target="blank">Go to proj</a>
+      <a href={`${project.url}`} target="blank">Check it out</a>
       </div>
 </div>
         
