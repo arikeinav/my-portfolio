a5ac0fe48b6ef9afa52270e40cd26bcd83f7723a
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-11-08 17:33:33 +0200

    last

--- src/cmps/Contact.jsx
+++ src/cmps/Contact.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React ,{Component} from 'react';
 import emailjs from "emailjs-com";
 import TextField from "@material-ui/core/TextField";
-import InputLabel from "@material-ui/core/InputLabel";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
 import { init } from "emailjs-com";
+import {AiFillLinkedin,AiFillGithub} from "react-icons/ai"
+import {BiPhone} from "react-icons/bi"
+import {FiMail} from "react-icons/fi"
+
+
 init("user_DwELl4XwTINN7NjVkso43");
 
-export function Contact() {
-  function sendEmail(e) {
-    e.preventDefault();
+export class Contact extends Component {
+  state = {
+      name: "",
+      email: "",
+      msg:"",
+      userMsg:""
+    
+  };
+  handleChange = (ev) => {
+    this.setState({userMsg:""})
+    const { name, value } = ev.target;
+    this.setState((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }))
+    
+  };
 
-    emailjs
+
+  sendEmail= async (e)=> {
+    e.preventDefault();
+    const { email ,msg } = this.state
+    if (!email || !msg) {
+      return this.setState({ userMsg: "Please enter email/Messege" });
+    }
+    await emailjs
       .sendForm(
         "service_sn03wmr",
         "template_2acjgim",
@@ -23,18 +47,30 @@ export function Contact() {
         },
         (error) => {
           console.log(error.text);
-        }
-      );
+        },
+       
+      )
+      this.setState({name: "", email: "", msg:"",userMsg:""})
   }
 
+render (){
   return (
-      
+      <div className="contact flex space-around" >
+        <div className="social flex column space-around align-start">
+      <div className="flex align-center" style={{color:'white', fontSize:'18px'}}> <BiPhone style={{marginRight:'8px'}}/>+972-545-311-920</div>
+      <div className="flex align-center" style={{color:'white', fontSize:'18px'}}> <FiMail style={{marginRight:'8px'}}/>arikeinav@gmail.com</div>
+          <a href="https://github.com/arikeinav" target="blank" className="flex align-center " style={{color:"white", fontSize:'25px',cursor:"pointer"}}><AiFillGithub style={{color:'white', fontSize:'25px',marginRight:'8px'}}/> <span>
+          Github</span></a>
+          <a href="www.linkedin.com/in/arik-einav" target="blank" className="flex align-center " style={{color:'rgb(10, 102, 194)', fontSize:'25px',cursor:"pointer"}}><AiFillLinkedin style={{color:'rgb(10, 102, 194)', fontSize:'25px',marginRight:'8px'}}/> <span> Linkedin</span></a>
+          
+          </div>
           
     <form
       className="contact-form"
-      onSubmit={sendEmail}
+      onSubmit={this.sendEmail}
     >
         <h1>Contact me</h1>
+        {this.state.userMsg &&<div className="error-msg"> <h5>{this.state.userMsg}</h5></div>} 
       <input type="hidden" name="contact_number" />
 
       <TextField
@@ -44,6 +80,8 @@ export function Contact() {
         color="blue"
         name="name"
         className="input"
+        value={this.state.name}
+        onChange={this.handleChange}
         
       />
       <TextField
@@ -53,6 +91,8 @@ export function Contact() {
         color="blue"
         name="email"
         className="input"
+        value={this.state.email}
+        onChange={this.handleChange}
       />
 
       <TextField
@@ -61,13 +101,18 @@ export function Contact() {
         multiline
         rows={4}
         variant="outlined"
-        name="message"
+        name="msg"
         className="text"
         color="blue"
+        value={this.state.msg}
+        onChange={this.handleChange}
       />
-      <input type="submit" value="Send" />
+      <input className="contact-btn" type="submit" id="btnsubmit"
+      
+      value="Send" />
     </form>
-    
+    </div>
   );
+}
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,47 +2,68 @@ import React, { Component } from "react";
 import { List } from "../cmps/List";
 import { About } from "../pages/About";
 // import { Navbar } from './cmps/Navbar'
-import Sky from 'react-sky';
-import react from '../styles/images/react.png'
-import html from '../styles/images/html.png'
-import sass from '../styles/images/sass.png'
-import {Contact} from '../cmps/Contact'
-
-
+import Sky from "react-sky";
+import react from "../styles/images/react.png";
+import html from "../styles/images/html.png";
+import js from "../styles/images/js.png";
+import css from "../styles/images/css.png";
+import { Contact } from "../cmps/Contact";
+import { Avatar } from "@material-ui/core";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { AiOutlineCopyright } from "react-icons/ai";
 
 export class Home extends Component {
   state = {
     projects: [
-      { name: "TASKIT", desc: "A Full Stack Web Developer, passionate about solving challenges, with experience in writing single-page-applications using the latest WEB technologies.", date: "14/07/2020",imgUrlName:'taskit',url:'https://arikeinav.github.io/Meme-Generator/' },
-      { name: "MEME", desc: "A Full Stack Web Developer, passionate about solving challenges, with experience in writing single-page-applications using the latest WEB technologies", date: "30/07/2020",imgUrlName:'meme',url:'https://arikeinav.github.io/Meme-Generator/'},
-      { name: "MINESWIPER", desc: "A Full Stack Web Developer, passionate about solving challenges, with experience in writing single-page-applications using the latest WEB technologies.", date: "14/07/2020",imgUrlName:'mines',url:'https://arikeinav.github.io/Minesweeper/' },
+      {
+        name: "TASKIT",
+        desc:
+          "Taskit is a collaboration tool that organizes your projects into boards. In one glance, Taskit tells you what's being worked on, who's working on what, and where something is in a process. The Taskit app is an end-to-end project based on React in the frontend and Node and MongoDB Atlas (serverless DB) at the backend.",
+        date: "14/07/2020",
+        imgUrlName: "taskit",
+        url: "https://arikeinav.github.io/Meme-Generator/",
+      },
+      {
+        name: "MEMEGEN",
+        desc:
+          "My Meme Generator allows you to add custom text to images. Using HTML5 Canvas, your images are created instantly on your own device. Edit, customize it and send it to your friends!!!",
+        date: "30/07/2020",
+        imgUrlName: "meme",
+        url: "https://arikeinav.github.io/Meme-Generator/",
+      },
+      {
+        name: "MINESWIPER",
+        desc:
+          "My take on the classic famous game. this is my very first project and its designed for desktop only. Try it, its fun!!!",
+        date: "14/07/2020",
+        imgUrlName: "mines",
+        url: "https://arikeinav.github.io/Minesweeper/",
+      },
       // { name: "Email-App", desc: "nice game", date: "14/07/2020",imgUrl:'../styles/images/mail.PNG' },
     ],
   };
 
   render() {
-    const p1 = "{"
-    const p5 = "}"
+    const p1 = "{";
+    const p5 = "}";
+
     return (
-        
-      <div className="main-app" style={{position:'relative',maxHeight:'17000px'}}>
-                <Sky
-    images={{
-      
-      0: react,  
-     1: html,  
-     2:sass
-    
-    }}
-    how={600} 
-    time={100}
-    size={'13px'} 
-    background={'#2c2929'} 
-    
-    
-    
-  />
-          
+      <div
+        className="main-app"
+        style={{ position: "relative", maxHeight: "17000px" }}
+      >
+        <Sky
+          images={{
+            0: react,
+            1: html,
+            2: js,
+            3: css,
+          }}
+          how={600}
+          time={100}
+          size={"13px"}
+          background={"#2c2929"}
+        />
 
         <div className="navbar">
           <div className="nav-tag flex align-center space-between">
@@ -50,39 +71,75 @@ export class Home extends Component {
             <a href="#Projects">PROJECTS</a>
             <a href="#tech">STACKS</a>
             <a href="#about">ABOUT</a>
-            <a href="https://docdro.id/dPkKK9b" target="blank">RESUME</a>
+            <a href="https://docdro.id/dPkKK9b" target="blank">
+              RESUME
+            </a>
           </div>
         </div>
-        <div id="home" className="home flex justify-center"> 
-     <h1 style={{position:'absolute',top:'100px',color:'white'}}>Welcome to my portfolyo</h1>
-      <div className="home-about flex">
-      <div className="my-object">
-  <p><span style={{color: '#5656ba',fontWeight: 'bolder'}}>const </span>fullStackDeveloper =<span className="bracets" style={{top: '3px'}}>{p1}</span></p>
-  <p><span style={{color:'#09d5f0'}}>name: </span>Arik Einav,</p>
-       <p><span style={{color:'#09d5f0'}}>age: </span>39,</p>
-       <p><span style={{color:'#09d5f0'}}>professione: </span>Full Stack Developer</p>
-       <p className="bracets" >{p5}</p>
-       </div>
-      
-       <img src={require("../styles/images/me.png")} alt="" />
-      </div>
-      {/* <a id="section02" href="#Projects"><span></span> PROJECTS</a> */}
-      </div>
-      <div className="projects" id="Projects">
-        <div style={{height:'80px'}}></div>
-          <List projects={this.state.projects} />
-          </div>
-       
-      
-        <div className="tech" id="tech" >
-  <div style={{height:'80px'}}></div>
- 
-          <h1>STACKS</h1>
-        <div style={{backgroundColor:'orange',width:'300px',height:'2px',margin:'auto'}}></div>
-          <div className="tech-list grid" >
+        <div id="home" className="home flex justify-center">
+          <h1
+            style={{
+              position: "absolute",
+              top: "100px",
+              color: "#adbbc8",
+              fontFamily: "Courier",
+            }}
+          >
+            Welcome to my portfolyo
+          </h1>
+          <div className="home-about flex">
+            <div className="my-object">
+              <p>
+                <span style={{ color: "#5656ba", fontWeight: "bolder" }}>const{" "} 
+                </span>
+                fullStackDeveloper =
+                <span className="bracets" style={{ top: "3px" }}>
+                  {p1}
+                </span>
+              </p>
+              <p>
+                <span style={{ color: "#09d5f0" }}>name: </span>Arik Einav,
+              </p>
+              <p>
+                <span style={{ color: "#09d5f0" }}>professione: </span>Full
+                Stack Developer
+              </p>
+              <p className="bracets">{p5}</p>
+            </div>
 
+            <div className="avatar">
+              <Avatar
+                src={require("../styles/images/me.png")}
+                style={{ height: "100%", width: "100%" }}
+              />
+            </div>
+            
+          </div>
+          
+        </div>
+        <div className="projects" id="Projects">
+          <div style={{ height: "80px" }}></div>
+          <List projects={this.state.projects} />
+        </div>
+
+        <div className="tech" id="tech">
+          <div style={{ height: "80px" }}></div>
+
+          <h1>STACKS</h1>
+          <div
+            style={{
+              backgroundColor: "orange",
+              width: "300px",
+              height: "2px",
+              margin: "auto",
+            }}
+          ></div>
+          <div className="tech-list grid">
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(3, 194, 241, 0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(3, 194, 241, 0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -96,7 +153,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(233, 62, 48, 0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(233, 62, 48, 0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -110,7 +170,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(190, 96, 139, 0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(190, 96, 139, 0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -124,7 +187,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(63, 185, 132,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(63, 185, 132,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -138,7 +204,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(247, 224, 24,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(247, 224, 24,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -152,7 +221,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(128, 189, 2,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(128, 189, 2,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -166,7 +238,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(85, 60, 123,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(85, 60, 123,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -180,7 +255,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(69, 165, 57,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(69, 165, 57,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -194,7 +272,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(115, 72, 177,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(115, 72, 177,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -208,7 +289,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(27, 135, 199,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(27, 135, 199,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -222,7 +306,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(0, 0, 0,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -236,7 +323,10 @@ export class Home extends Component {
               </div>
             </div>
             <div className="flip-card">
-              <div className="flip-card-inner" style={{boxShadow:'0 4px 8px 0 rgba(213, 0, 0,0.13)'}}>
+              <div
+                className="flip-card-inner"
+                style={{ boxShadow: "0 4px 8px 0 rgba(213, 0, 0,0.13)" }}
+              >
                 <div className="flip-card-front">
                   <img
                     className="tech-img"
@@ -251,18 +341,45 @@ export class Home extends Component {
             </div>
           </div>
         </div>
-        <div style={{height:'80px'}}></div>
-        <h1 style={{color:'white'}}>ABOUT</h1>
-       
-       <div style={{backgroundColor:'orange',width:'300px',height:'2px',margin:'auto'}}></div>
-          <div id="about" className="about flex space-between align-center">
-          <img src={require("../styles/images/me.png")} alt="" style={{height:'200px',marginTop:'0'}} />
-          <About  />
-          <Contact/>
+        <div style={{ height: "80px" }}></div>
+        <h1 style={{ color: "white" }}>ABOUT</h1>
+
+        <div
+          style={{
+            backgroundColor: "orange",
+            width: "300px",
+            height: "2px",
+            margin: "auto",
+          }}
+        ></div>
+        <div id="about" className="about flex space-between align-center">
+        
+          <div className="about-avatar">
+              <Avatar
+                src={require("../styles/images/me.png")}
+                style={{ height: "150px", width: "150px", marginTop: "0" }}
+              />
+            </div>
+          <About />
         </div>
+        <Contact />
+        <footer className="flex justify-center" style={{ height: "55px", color: "white", background: "black" }}>
         
-      <footer><div style={{height:'100px'}}></div></footer>
-        
+          <AiOutlineCopyright style={{marginRight:'5px',alignSelf:'center'}}/>
+          <p
+            className="fotter"
+            style={{ padding: "0", fontFamily: "Courier", margin: "0",alignSelf:'center' }}
+            
+            >All rights reserved Arik Einav
+          </p>
+        </footer>
+        <a
+          href="https://api.whatsapp.com/send?phone=972545311920"
+          className="whatsup"
+          target="blank"
+        >
+          <FaWhatsappSquare />
+        </a>
       </div>
     );
   }
